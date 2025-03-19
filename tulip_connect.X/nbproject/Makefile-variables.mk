53808a121b7fe446bae09b233d005ab83fa25c55
JelleBeertenKleventa <j.beerten@kleventa.nl>
2025-03-19 12:04:41 +0100

    git ignore update
    new gitignore and multiple files inclusion due to incorrect branch switching

---
+++ tulip_connect.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=tulip_connect.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/tulip_connect.X.production.hex
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=tulip_connect.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/tulip_connect.X.production.hex
