6f0d6338a9076d8918eba48156b42234ed7e0cee
JelleBeertenKleventa <j.beerten@kleventa.nl>
2025-03-21 14:48:54 +0100

    active mode swtich case

--- src/app_active_mode_controller.c
+++ src/app_active_mode_controller.c
@@ -14,6 +14,52 @@ extern APP_ACTIVE_MODE_CONTROLLER_STATES app_active_mode_controllerState;
 extern APP_ACTIVE_MODE_CONTROLLER_DATA app_active_mode_controllerData;
 
 
+void callActiveModeTaskHandler() {
+    switch(app_active_mode_controllerData.currentRunningMode)
+    {
+        case HEATING:{
+            // Call heating tasks;
+            break;
+        }
+        
+        case COOLING:{
+            break;
+        }
+        
+        case FLOOR_HEATING: {
+            // Zal momenteel nog niks doen, reset alles als dit wordt gecalled
+            APP_ACTIVE_MODE_CONTROLLER_Initialize();
+            break;
+        }
+            
+        case HOT_WATER:{
+            break;
+        }
+        
+        case HOT_WATER_COOLING:{
+            break;
+        }
+        
+        case HOT_WATER_HEATING:{
+            break;
+        }
+        
+        case HOT_WATER_FLOOR_HEATING:{
+             // Zal momenteel nog niks doen, reset alles als dit wordt gecalled
+            APP_ACTIVE_MODE_CONTROLLER_Initialize();
+            break;
+        }
+              
+        default:{
+            APP_ACTIVE_MODE_CONTROLLER_Initialize();
+            break;
+        }
+    }
+    return;
+}
+
+
+
 void APP_ACTIVE_MODE_CONTROLLER_Initialize ( void )
 {
     app_active_mode_controllerData.currentRunningMode = HEATING;
@@ -26,7 +72,7 @@ void APP_ACTIVE_MODE_CONTROLLER_Initialize ( void )
 
 
 void APP_ACTIVE_MODE_CONTROLLER_Tasks ( void )
-{
+{    
     switch ( app_active_mode_controllerState )
     {
         /* Application's initial state. */
@@ -40,12 +86,20 @@ void APP_ACTIVE_MODE_CONTROLLER_Tasks ( void )
         
         case APP_ACTIVE_MODE_CONTROLLER_STATE_SERVICE_TASKS:
         {
+           
+            if (app_active_mode_controllerData.currentRunningMode != app_active_mode_controllerData.previousRunningMode) {
+                // Reset possible running states.              
+            }
+           
+            callActiveModeTaskHandler();
+            
             break;
         }
 
 
         default:
         {
+            APP_ACTIVE_MODE_CONTROLLER_Initialize();
             break;
         }
     }
