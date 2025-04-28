993a1752db538fdc754f479ac70791de6691c4a7
KleventaEltron <s.kolkman@kleventa.nl>
2025-04-28 10:28:47 +0200

    Cooling added

--- src/files/cooling_mode.h
+++ src/files/cooling_mode.h
@@ -6,12 +6,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+    
+#include <stddef.h>                    
+#include <stdbool.h>                   
+#include <stdlib.h>                   
+#include <string.h>
+#include <stdio.h>
+#include "definitions.h" 
 
        
 void COOLING_MODE_Initialize ( void );
 void COOLING_MODE_Tasks ( void );
    
-    
+const char * getCoolingStateToString();    
     
 
 #ifdef __cplusplus
