e9e4bd0d96ce25f25116da8c8d2f113f1cf8a133
JelleBeertenKleventa <j.beerten@kleventa.nl>
2025-06-04 11:03:56 +0200

    updated file version

--- src/config/default/user.h
+++ src/config/default/user.h
@@ -37,7 +37,7 @@ extern "C" {
 #define TEMPERATURE_ALARM_VALUE -9999    
     
 //     
-#define THIS_FIRMWARE_VERSION 2000001 // 2.0.0
+#define THIS_FIRMWARE_VERSION 2000001 // 2.0.1
     
 //#define SECONDS_IN_DAY 86400 // 1 day
 //#define SECONDS_IN_DAY 60 // 1 minute
